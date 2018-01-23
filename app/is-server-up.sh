be916be5b9918e32f2d755b85a56e4380bd2ebe2
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-23 19:13:24 +0000

    Added cmd line flags and general refurbish

--- app/is-server-up.sh
+++ app/is-server-up.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
-if curl 172.17.0.1:3000; then
+#!/bin/ksh 
+#        1         2         3         4         5         6         7         8         9
+#234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+################################################################################
+SCRIPT_NAME="is-server-up"
+################################################################################
+VERSION="0.01a"
+AUTHOR="Orlando Hehl Rebelo dos Santos"
+DATE_INI="23-01-2018"
+DATE_END="21-01-2018"
+################################################################################
+#Changes:
+#
+
+# 172.17.0.1 for container use only
+
+HOST=127.0.0.1
+PORT=3000
+
+usage(){
+        echo $SCRIPT_NAME
+        echo "Usage: $SCRIPT_NAME.sh [-h host] [-p port]"
+        echo "  -h   Host name, default is 127.0.0.1"
+        echo "  -h   TCP Port, default is 3000"
+}
+
+while getopts "h:p:" arg
+do
+        case $arg in
+            h)  
+                HOST=$OPTARG
+                ;;  
+            p)  
+                PORT=$OPTARG
+                ;;  
+           *)
+                usage
+                exit 1
+                ;;
+        esac
+done
+
+shift $(($OPTIND - 1))
+
+if curl ${HOST}:${PORT}; then
    exit 0
 else
-    exit 1
+   exit 1
 fi
