537df9ca5308a46f8b9ec138125fc8e4aa9b5dbe
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-24 16:29:27 +0000

    Implementacao das novas mensagens do deploy-prod

--- app/deploy-prod.sh
+++ app/deploy-prod.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+echo ">>> deply-prod v:0.2a"
 cd app
 
 action=${1}
 
 HOST=ec2-54-91-147-118.compute-1.amazonaws.com
+APP_PORT=3000
 
 function stop {
+    echo ">>> Parando container sciensa-app-prod ..."
     if docker stop -t3 sciensa-app-prod; then
+       echo ">>> Container parado com sucesso!"
+       echo ">>> Removendo imagem ..."
         docker rmi -f ohrsan/node-sciensa-prj:prod || exit 2
     else
         exit 1
@@ -15,8 +19,11 @@ function stop {
 }
 
 function start {
+    echo ">>> Construindo a imagem com o codigo atualizado..."
     if docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:prod .; then
-        APP_ENV=PROD PUBLIC_DNS=$HOST docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p 3000:3000 -p 3001:3001 -v /var/www  --name sciensa-app-prod ohrsan/node-sciensa-prj:prod || exit 4
+        echo ">>> Imagem contruida com sucesso!"
+        echo ">>> Inicializando container sciensa-app-prod $HOST:$APP_PORT"
+        APP_ENV=PROD PUBLIC_DNS=$HOST docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p $APP_PORT:3000 -p 3001:3001 -v /var/www  --name sciensa-app-prod ohrsan/node-sciensa-prj:prod || exit 4
     else
         exit 3
     fi
@@ -24,12 +31,24 @@ function start {
 
 case $action in
     stop )
+        echo "/*------------------------------*/"
+        echo "   ***   ENCERRANDO $APP_ENV ***"
+        echo "/*------------------------------*/"
+
         stop
         ;;
     start )
+        echo "/*------------------------------*/"
+        echo "   ***   INICIALIZANDO $APP_ENV ***"
+        echo "/*------------------------------*/"
+
         start
         ;;
     restart )
+        echo "/*------------------------------*/"
+        echo "   ***   REINICIANDO $APP_ENV ***"
+        echo "/*------------------------------*/"
+
         stop
         start
         ;;
