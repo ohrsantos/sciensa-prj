72d786f006ab2dfc5f5ae36dcbf9a07d535eaf62
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-24 20:45:17 +0000

    Corrigido erro de sintaxe

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROD_HOST=${1}
 
-ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh update PROD $PROD_HOST 3000
+ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh update PROD $PROD_HOST 3000"
 #ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy-prod.sh restart"
