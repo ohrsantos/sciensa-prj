c80a930e2ae4378038fb4e1cf707560709c2197e
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-24 16:09:37 +0000

    Implementadas novas mensagens de log para o deploy do dev

--- app/deploy-dev.sh
+++ app/deploy-dev.sh
@@ -7,7 +7,10 @@ action=${1}
 HOST=ec2-34-239-132-79.compute-1.amazonaws.com
 
 function stop {
+    echo "Parando container sciensa-app-dev ..."
     if docker stop -t3 sciensa-app-dev; then
+    echo "Container parado com sucesso!"
+    echo "Removendo imagem ..."
         docker rmi -f ohrsan/node-sciensa-prj:dev || exit 2
     else
         exit 1
@@ -15,7 +18,10 @@ function stop {
 }
 
 function start {
+    echo "Construindo a imagem com o codigo atualizado..."
     if docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:dev .; then
+        echo "Imagem contruida com sucesso!"
+        echo "Inicializando container sciensa-app-dev na porta $PORTA..."
         APP_ENV=DEV PUBLIC_DNS=$HOST docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p 3000:3000 -p 3001:3001 -v /var/www  --name sciensa-app-dev ohrsan/node-sciensa-prj:dev || exit 4
     else
         exit 3
@@ -24,12 +30,21 @@ function start {
 
 case $action in
     stop )
+        echo "/*------------------------------*/"
+        echo "   ***   ENCERRANDO ***"
+        echo "/*------------------------------*/"
         stop
         ;;
     start )
+        echo "/*------------------------------*/"
+        echo "   ***   INICIALIZANDO ***"
+        echo "/*------------------------------*/"
         start
         ;;
     restart )
+        echo "/*------------------------------*/"
+        echo "   ***   REINICIANDO ***"
+        echo "/*------------------------------*/"
         stop
         start
         ;;
