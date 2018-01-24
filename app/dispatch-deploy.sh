3d4455a022870161ca945cac803fd07317eae800
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-24 10:33:31 +0000

    Atualizacao de DNS

--- app/dispatch-deploy.sh
+++ app/dispatch-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PROD_HOST=ec2-54-221-138-158.compute-1.amazonaws.com
+PROD_HOST=ec2-54-91-147-118.compute-1.amazonaws.com
 
 ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy-prod.sh restart"
