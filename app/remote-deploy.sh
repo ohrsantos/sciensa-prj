603a43098393f32baaecdd4492ed9a7caf19728e
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-24 21:00:07 +0000

    Recebendo sua parametrizacao pelo Jenkins

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-PROD_HOST=${1}
 
-ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh update PROD $PROD_HOST 3000"
-#ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy-prod.sh restart"
+action=${1}
+APP_ENV=${2}
+PROD_HOST=${3}
+PORT=${4}
+
+ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh $action $APP_ENV $PROD_HOST $PORT"
