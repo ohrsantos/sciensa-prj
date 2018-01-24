0300f0b29f78eeedfbd7e9b3c563db463e431283
ohrsantos <orlando.hrsantos@gmail.com>
2018-01-24 00:39:33 +0000

    Refatorado conforme deply-dev

--- app/deploy-prod.sh
+++ app/deploy-prod.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+cd app
+
 action=${1}
+
+HOST=ec2-54-89-229-198.compute-1.amazonaws.com
+
+function stop {
+    if docker stop -t3 sciensa-app-prod; then
+        docker rmi -f ohrsan/node-sciensa-prj:prod || exit 2
+    else
+        exit 1
+    fi
+}
+
+function start {
+    if docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:prod .; then
+        APP_ENV=PROD PUBLIC_DNS=$HOST docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p 3000:3000 -p 3001:3001 -v /var/www  --name sciensa-app-prod ohrsan/node-sciensa-prj:prod || exit 4
+    else
+        exit 3
+    fi
+}
 
 case $action in
     stop )
-        docker stop -t5 sciensa-app-prod
-        docker rmi -f ohrsan/node-sciensa-prj:prod
+        stop
         ;;
     start )
-        docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:prod .
-        APP_ENV=PROD PUBLIC_DNS=ec2-54-89-229-198.compute-1.amazonaws.com docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p 3000:3000 -p 3001:3001 -v /var/www  --name sciensa-app-prod ohrsan/node-sciensa-prj:prod
+        start
         ;;
     restart )
-        docker stop -t5 sciensa-app-prod
-        docker rmi -f ohrsan/node-sciensa-prj:prod
-        docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:prod .
-        APP_ENV=PROD PUBLIC_DNS=ec2-54-89-229-198.compute-1.amazonaws.com docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p 3000:3000 -p 3001:3001 -v /var/www  ohrsan/node-sciensa-prj:prod  --name sciensa-app-prod
+        stop
+        start
         ;;
     *)
          echo "Opcao invalida!"
         ;;
 esac
+exit 0
