2999621bd030c3e52b90eb871b712fb46c6b0d50
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-25 13:39:50 -0200

    INITIALIZE action added

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ">>>> remote-deploy v:0.20a"
+echo ">>>> remote-deploy v:0.3.0a"
 
 action=$(echo ${1} | tr '[:lower:]' '[:upper:]')
 APP_ENV=${2}
@@ -13,6 +13,20 @@ docker login -u=ohrsan -p=bomdia01
 docker push ohrsan/node-sciensa-prj:DEV
 
 case $action in
+    "INITIALIZE" )
+        echo "----------------------------------"
+        echo "      INICIALIZANDO $APP_ENV "
+        echo "----------------------------------"
+        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+                "docker pull node:latest \
+                 PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
+                 docker run -d --rm \
+                     -e APP_ENV -e PUBLIC_DNS \
+                     -p $PORT:3000 -p 3001:3001 \
+                     -v /var/www  \
+                     --name sciensa-app-PROD \
+                     ohrsan/node-sciensa-prj:DEV"
+        ;;
     "STOP" )
         echo "----------------------------------"
         echo "      ENCERRANDO $APP_ENV "
