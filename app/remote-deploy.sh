50bcf070f694f6bb164fc2a0ad3991e4224b74ab
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-25 01:35:05 +0000

    code clean up

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -8,5 +8,14 @@ PORT=${4}
 docker login -u=ohrsan -p=bomdia01
 
 docker push ohrsan/node-sciensa-prj:DEV
-ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "docker stop -t3 sciensa-app-PROD; docker rmi ohrsan/node-sciensa-prj:DEV; PUBLIC_DNS=$PROD_HOST APP_ENV=PROD docker run -d --rm -e APP_ENV -e PUBLIC_DNS -p $PORT:3000 -p 3001:3001 -v /var/www  --name sciensa-app-PROD ohrsan/node-sciensa-prj:DEV"
-#ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh $action $APP_ENV $PROD_HOST $PORT"
+
+ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+        "docker stop -t3 sciensa-app-PROD; \
+         docker rmi ohrsan/node-sciensa-prj:DEV; \
+         PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
+         docker run -d --rm \
+             -e APP_ENV -e PUBLIC_DNS \
+             -p $PORT:3000 -p 3001:3001 \
+             -v /var/www  \
+             --name sciensa-app-PROD \
+             ohrsan/node-sciensa-prj:DEV"
