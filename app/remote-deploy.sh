5fab113c4d91d6767204fbd28c4d018633ef0428
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-25 13:22:47 -0200

    Implemented START, STOP beside UPDATE action

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 
-action=${1}
+echo ">>>> remote-deploy v:0.20a"
+
+action=$(echo ${1} | tr '[:lower:]' '[:upper:]')
 APP_ENV=${2}
 PROD_HOST=${3}
 PORT=${4}
-
-if [[ $action != "UPDATE" ]]; then
-    echo "Action: $action"
-    exit 0
-fi
 
 
 docker login -u=ohrsan -p=bomdia01
 
 docker push ohrsan/node-sciensa-prj:DEV
 
-ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
-        "docker stop -t3 sciensa-app-PROD; \
-         docker rmi ohrsan/node-sciensa-prj:DEV; \
-         PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
-         docker run -d --rm \
-             -e APP_ENV -e PUBLIC_DNS \
-             -p $PORT:3000 -p 3001:3001 \
-             -v /var/www  \
-             --name sciensa-app-PROD \
-             ohrsan/node-sciensa-prj:DEV"
+case $action in
+    "STOP" )
+        echo "----------------------------------"
+        echo "      ENCERRANDO $APP_ENV "
+        echo "----------------------------------"
+        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+                "docker stop -t3 sciensa-app-PROD; \
+                 docker rmi ohrsan/node-sciensa-prj:DEV"
+        ;;
+    "START" )
+        echo "----------------------------------"
+        echo "      INICIALIZANDO $APP_ENV "
+        echo "----------------------------------"
+        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+                "PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
+                 docker run -d --rm \
+                     -e APP_ENV -e PUBLIC_DNS \
+                     -p $PORT:3000 -p 3001:3001 \
+                     -v /var/www  \
+                     --name sciensa-app-PROD \
+                     ohrsan/node-sciensa-prj:DEV"
+        ;;
+    "UPDATE" )
+        echo "----------------------------------"
+        echo "      DEPLOYING $APP_ENV "
+        echo "----------------------------------"
+        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+                "docker stop -t3 sciensa-app-PROD; \
+                 docker rmi ohrsan/node-sciensa-prj:DEV; \
+                 PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
+                 docker run -d --rm \
+                     -e APP_ENV -e PUBLIC_DNS \
+                     -p $PORT:3000 -p 3001:3001 \
+                     -v /var/www  \
+                     --name sciensa-app-PROD \
+                     ohrsan/node-sciensa-prj:DEV"
+        ;;
+    *)
+        echo "Action: $action"
+        exit 0
+        ;;
+esac
