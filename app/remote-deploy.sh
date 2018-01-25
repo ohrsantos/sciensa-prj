f14bc35b7fdf82602a13cbf8549f86cc776223e3
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-25 00:57:58 +0000

    Alteracao do processo de deployment remoto para apenas publicar a imagem de desemvolvimento e roda-la na producao devidamente parametrizada.

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -5,4 +5,5 @@ APP_ENV=${2}
 PROD_HOST=${3}
 PORT=${4}
 
-ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh $action $APP_ENV $PROD_HOST $PORT"
+docker push ohrsan/ohrsan/node-sciensa-prj:DEV
+#ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh $action $APP_ENV $PROD_HOST $PORT"
