7e3c6c196676e684a42368cf9876329f968e74f0
EC2 Default User <ec2-user@ip-172-31-84-200.ec2.internal>
2018-01-25 01:12:37 +0000

    Here we go again!

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -5,7 +5,7 @@ APP_ENV=${2}
 PROD_HOST=${3}
 PORT=${4}
 
-docker login --username=ohrsan
+docker login -u=ohrsan -p=bomdia01
 
 docker push ohrsan/node-sciensa-prj:DEV
 #ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< "cd /home/ec2-user/deploy/sciensa-prj/; app/deploy.sh $action $APP_ENV $PROD_HOST $PORT"
