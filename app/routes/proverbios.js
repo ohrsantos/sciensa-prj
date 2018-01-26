43a5284d41ef095e1443c1e51df42b3b7c1fdb3f
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-26 00:23:44 -0200

    ...

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
         //var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
 
         //ProverbiosDAO.lista(function(err, results) {
-            res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV, app_ver: app_ver});
+            res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV, app_ver: app_version});
         //});
 
         //connection.end();
