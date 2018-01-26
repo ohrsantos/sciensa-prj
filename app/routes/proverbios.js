2ded869441e4466d4f396c591c86e34f61d56fad
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-26 12:36:43 -0200

    Criado um switch para tratar dev/prod pelo app load balancer

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,13 +1,29 @@
 module.exports = function(app) {
     var APP_VERSION = '0.02.007a';
-    app.get('/dev', function(req, res) {
-            res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV,
-                                                app_version: APP_VERSION,
-                                                process_pid: process.pid,
-                                                process_arch: process.arch,
-                                                process_platform: process.platform
-                                               });
-    });
+    switch(process.APP_ENV) {
+    case "dev":
+            app.get('/dev', function(req, res) {
+                    res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV,
+                                                        app_version: APP_VERSION,
+                                                        process_pid: process.pid,
+                                                        process_arch: process.arch,
+                                                        process_platform: process.platform
+                                                       });
+            });
+            break;
+
+    case "prod":
+            app.get('/dev', function(req, res) {
+                    res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV,
+                                                        app_version: APP_VERSION,
+                                                        process_pid: process.pid,
+                                                        process_arch: process.arch,
+                                                        process_platform: process.platform
+                                                       });
+            });
+            break;
+    }
+
     app.get('/proverbios', function(req, res) {
         var connection = app.infra.connectionFactory();
         var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
