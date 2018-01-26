5b219611b8e276da337ca70a3d7b0c3bce222818
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-25 22:19:29 -0200

    Initial Commit

---
+++ app/infra/connectionFactory.js
@@ -0,0 +1,22 @@
+var mysql  = require('mysql');
+
+var db_host="sciensa-db-instance.cbfgofnkvvl3.us-east-1.rds.amazonaws.com";
+
+if (process.env.APP_ENV == "DEV") 
+   var database="sciensa_dev";
+
+if (process.env.APP_ENV == "PROD") 
+   var database="sciensa_prod";
+
+function createDBConnection() {
+    return mysql.createConnection({
+        host: db_host,
+        user: 'root',
+        password: 'sciensa1',
+        database: database
+    });
+}
+
+module.exports = function() {
+    return createDBConnection;
+}
