1a2df346bdf8d0a0edde33f0d35398f5eb165f80
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-26 09:34:08 -0200

    Adicionada variavel APP_VERSION

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,11 +1,11 @@
 module.exports = function(app) {
-
+    var APP_VERSION = '0.02.003a';
     app.get('/', function(req, res) {
         //var connection = app.infra.connectionFactory();
         //var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
 
         //ProverbiosDAO.lista(function(err, results) {
-            res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV, app_version: '0.02.002a'});
+            res.render('proverbios/index.ejs', {app_env: process.env.APP_ENV, app_version: APP_VERSION});
         //});
 
         //connection.end();
