ce6537712ed647485c017434577b5832b2b2bdf8
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-25 23:08:32 -0200

    proverbioDAO para ProverbioDAO

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     app.get('/proverbios', function(req, res) {
         var connection = app.infra.connectionFactory();
-        var proverbiosDAO = new app.infra.ProverbiosDAO(connection);
+        var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
 
         ProverbiosDAO.lista(function(err, results) {
             res.render('proverbios/lista', {lista: results});
