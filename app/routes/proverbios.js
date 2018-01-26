7795060d611dc6dcc047505b8e8552fdd789590b
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-25 23:57:11 -0200

    Initial Commit

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,4 +1,14 @@
 module.exports = function(app) {
+    app.get('/', function(req, res) {
+        //var connection = app.infra.connectionFactory();
+        //var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
+
+        //ProverbiosDAO.lista(function(err, results) {
+            res.render('proverbios/index.ejs');
+        //});
+
+        //connection.end();
+    });
     app.get('/proverbios', function(req, res) {
         var connection = app.infra.connectionFactory();
         var ProverbiosDAO = new app.infra.ProverbiosDAO(connection);
