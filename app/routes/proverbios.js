53efdc2a6c7e7dbf6c3faed35edab93fe00840df
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-28 10:52:59 -0200

    versao atualizada para teste

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.015a';
+    var APP_VERSION = '0.02.016a';
 
 function log_date_ip(req, path) {
         var ip;
