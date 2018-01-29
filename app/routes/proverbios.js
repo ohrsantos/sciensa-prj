53a2081589d4ef83cc0a0897e7b162f9ca526ccd
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-29 13:08:49 -0200

    .

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.020a';
+    var APP_VERSION = '0.02.021a';
 
 function log_date_ip(req, path) {
         var ip;
