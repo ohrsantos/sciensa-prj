72328136a4fd00a0ea6657f3fd41d9cf32c6ede3
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-30 09:55:17 -0200

    Na falha de parar o container o script continuara executando a remocao da imagem

--- app/deploy.sh
+++ app/deploy.sh
@@ -7,17 +7,15 @@ export APP_ENV=$(echo ${2} | tr '[:lower:]' '[:upper:]')
 export PUBLIC_DNS=${3}
 APP_PORT=${4}
 
-echo ">>>> deploy-${APP_ENV} v:0.33a"
+echo ">>>> deploy-${APP_ENV} v:0.34a"
 
 function stop {
     echo ">>>> Parando container sciensa-app-${APP_ENV} ..."
     if docker stop -t3 sciensa-app-${APP_ENV}; then
         echo ">>>> Container parado com sucesso!"
-        echo ">>>> Removendo imagem ..."
-        docker rmi -f ohrsan/node-sciensa-prj:${APP_ENV} || exit 2
-    else
-        exit 1
     fi
+    echo ">>>> Removendo imagem ..."
+    docker rmi -f ohrsan/node-sciensa-prj:${APP_ENV} || exit 1
 }
 
 function start {
@@ -25,15 +23,15 @@ function start {
         echo ">>>> Codigos da aplicacao atualizados com sucesso!"
     else
         echo "git pull, falhou!!!"
-        exit 5
+        exit 2
     fi
     echo ">>>> Construindo a imagem com o codigo atualizado..."
     if docker  build -f Dockerfile.sciensa-app -t ohrsan/node-sciensa-prj:${APP_ENV} .; then
         echo ">>>> Imagem construida com sucesso!"
         echo ">>>> Inicializando container sciensa-app-${APP_ENV} $HOST:$APP_PORT"
-        docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p $APP_PORT:3000 -p 3001:3001 -v /var/www  --name sciensa-app-${APP_ENV} ohrsan/node-sciensa-prj:${APP_ENV} || exit 4
+        docker run -d  --rm -e APP_ENV -e PUBLIC_DNS -p $APP_PORT:3000 -p 3001:3001 -v /var/www  --name sciensa-app-${APP_ENV} ohrsan/node-sciensa-prj:${APP_ENV} || exit 3
     else
-        exit 3
+        exit 4
     fi
 }
 
