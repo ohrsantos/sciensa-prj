240a722214a328ce97eeaf3acce593e9f2b7f02e
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-01-31 09:18:30 -0200

    Mudanca de versao para Teste de build

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.035a';
+    var APP_VERSION = '0.02.036a';
 
 function log_date_ip(req, path) {
         var ip;
