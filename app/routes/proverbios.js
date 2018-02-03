c6cf22c60e1e34dada3fa114bf8585b36aa85899
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-03 09:22:10 -0200

    Teste docker Jenkins

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.043a';
+    var APP_VERSION = '0.02.044a';
 
 function log_date_ip(req, path) {
         var ip;
