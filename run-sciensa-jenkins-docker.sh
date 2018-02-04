f1c4204ce9d01f8564ee18a25373447bb474756e
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-04 11:35:59 -0200

    Corrigida versao, que eh a 2

--- run-sciensa-jenkins-docker.sh
+++ run-sciensa-jenkins-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm -u root -v /var/run/docker.sock:/var/run/docker.sock  -v jenkins_docker_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --name dev-sciensa-jenkins-docker ohrsan/jenkins-docker-sciensa:v3
+docker run -d --rm -u root -v /var/run/docker.sock:/var/run/docker.sock  -v jenkins_docker_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --name dev-sciensa-jenkins-docker ohrsan/jenkins-docker-sciensa:v2
