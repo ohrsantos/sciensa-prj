f68a736f5e1fd22963406eeadc7529289f481573
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-04 11:32:54 -0200

    Mudado sciensa-jenkins-docker:v2

--- build-sciensa-jenkins-docker.sh
+++ build-sciensa-jenkins-docker.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker build -f Dockerfile.jenkins-docker-v3  -t ohrsan/jenkins-docker:v3 .
+docker build -f Dockerfile.sciensa-jenkins-docker-v2  -t sciensa-jenkins-docker:v2 .
+
+docker login -u=ohrsan -p=bomdia01
+
+docker push ohrsan/sciensa-jenkins-docker:v2
