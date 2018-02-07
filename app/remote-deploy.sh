814f2ade3618d000f18f633d124aa66b86b7a1ba
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-07 12:18:08 -0200

    Added -o "StrictHostKeyChecking no" to ssh command

--- app/remote-deploy.sh
+++ app/remote-deploy.sh
@@ -13,7 +13,7 @@ docker login -u=ohrsan -p=bomdia01
 docker push ohrsan/node-sciensa-prj:DEV
 
 function pull_node_latest_image {
-    ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+    ssh -o "StrictHostKeyChecking no" -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
             "if ! docker images | grep latest >/dev/null; then\
                  echo \"Pulling node:latest...\"; \
                  docker pull node:latest; \
@@ -26,7 +26,7 @@ case $action in
         echo "      INICIALIZANDO $APP_ENV "
         echo "----------------------------------"
         pull_node_latest_image
-        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+        ssh -o "StrictHostKeyChecking no" -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
                 "docker pull node:latest; \
                  PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
                  docker run -d --rm \
@@ -40,7 +40,7 @@ case $action in
         echo "----------------------------------"
         echo "      ENCERRANDO $APP_ENV "
         echo "----------------------------------"
-        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+        ssh -o "StrictHostKeyChecking no" -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
                 "docker stop -t3 sciensa-app-PROD; \
                  docker rmi ohrsan/node-sciensa-prj:DEV"
         ;;
@@ -49,7 +49,7 @@ case $action in
         echo "      INICIANDO $APP_ENV "
         echo "----------------------------------"
         pull_node_latest_image
-        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+        ssh -o "StrictHostKeyChecking no" -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
                 "PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
                  docker run -d --rm \
                      -e APP_ENV -e PUBLIC_DNS \
@@ -62,7 +62,7 @@ case $action in
         echo "----------------------------------"
         echo "      DEPLOYING $APP_ENV "
         echo "----------------------------------"
-        ssh -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
+        ssh -o "StrictHostKeyChecking no" -i ohrs-aws-key-file.pem ec2-user@$PROD_HOST <<< \
                 "docker stop -t3 sciensa-app-PROD; \
                  docker rmi ohrsan/node-sciensa-prj:DEV; \
                  PUBLIC_DNS=$PROD_HOST APP_ENV=PROD \
