c949f13664e7eb52da348aa3366f7164c884514c
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-13 14:04:47 -0200

    insert func created

--- app/infra/ProverbiosDAO.js
+++ app/infra/ProverbiosDAO.js
@@ -6,6 +6,10 @@ ProverbiosDAO.prototype.lista = function(callback) {
     this._connection.query('SELECT * FROM messages',callback);
 }
 
+ProdutosDAO.prototype.insert = function(proverbios, callback){
+    this._connection.query('INSERT INTO messages SET ?', proverbios, callback);
+}
+
 module.exports = function(){
     return ProverbiosDAO;
 };
