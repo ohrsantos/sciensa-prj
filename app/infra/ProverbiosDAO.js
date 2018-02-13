b09393a5b84b2a893af24ae62f87a9fdb4c7b255
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-13 14:51:22 -0200

    .

--- app/infra/ProverbiosDAO.js
+++ app/infra/ProverbiosDAO.js
@@ -8,6 +8,8 @@ ProverbiosDAO.prototype.lista = function(callback) {
 
 ProverbiosDAO.prototype.insert = function(proverbios, callback){
     this._connection.query('INSERT INTO messages SET ?', proverbios, callback);
+    //Essa seria a forma origina sem uma das ajudas do parser
+    //this._connection.query('insert into produtos (titulo, preco, descricao) values (?, ?, ?)',  [produto.titulo, produto.preco, produto.descricao], callback);
 }
 
 module.exports = function(){
