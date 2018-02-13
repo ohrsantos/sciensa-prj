990fa8c09f684f2a81327d072afeed0cb7f9070a
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-13 09:09:38 -0200

    .

--- containers/build-sciensa-jenkins-docker-v2.sh
+++ containers/build-sciensa-jenkins-docker-v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build -f Dockerfile.sciensa-jenkins-docker-v2  -t ohrsan/sciensa-jenkins-docker:v2 .
+docker build -f Dockerfile.sciensa-jenkins-docker-v3  -t ohrsan/sciensa-jenkins-docker:v3 .
 
 docker login -u=ohrsan -p=bomdia01
 
-docker push ohrsan/sciensa-jenkins-docker:v2
+docker push ohrsan/sciensa-jenkins-docker:v3
