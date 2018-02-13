f62f9ea819e9a961ce2e299d7b8bcecb95b22958
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-13 09:41:27 -0200

    Automatizando o tar para savamento do contexto do jenkins

--- containers/build-sciensa-jenkins-docker-v2.sh
+++ containers/build-sciensa-jenkins-docker-v2.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-docker build -f Dockerfile.sciensa-jenkins-docker-v3  -t ohrsan/sciensa-jenkins-docker:v3 .
 
-docker login -u=ohrsan -p=bomdia01
+SCIENSA_JENKINS_HOME=/var/lib/docker/volumes/jenkins_docker_home/_data
+WORK_DIR=$(pwd)
 
-docker push ohrsan/sciensa-jenkins-docker:v3
+cd $SCIENSA_JENKINS_HOME
+
+tar zcvf sciensa-jenkins.tar.gz *
+
+#This version is for the case with need hidden files
+#tar zcvf sciensa-jenkins.tar.gz * .??*
+
+mv sciensa-jenkins.tar.gz $WORK_DIR
+
+cd -
+
+#docker build -f Dockerfile.sciensa-jenkins-docker-v3  -t ohrsan/sciensa-jenkins-docker:v3 .
+
+#docker login -u=ohrsan -p=bomdia01
+
+#docker push ohrsan/sciensa-jenkins-docker:v3
