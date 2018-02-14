213349e40faf6d7802b09d7e5da50bbc4a84b9e6
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-14 08:48:03 -0200

    .

--- containers/dev.sciensa-prj.run-aws-instance.sh
+++ containers/dev.sciensa-prj.run-aws-instance.sh
@@ -132,9 +132,10 @@ for index in "${!user_data[@]}"; do
 done
 
 echo "Initializing instance..."
-new_instance_id=$($AWS --output json ec2 run-instances $INSTANCE_DRY_RUN --image-id  $INSTANCE_AMI_ID --count $INSTANCE_COUNT \
-                       --instance-type $INSTANCE_TYPE --key-name $INSTANCE_KEY_PAIR --security-groups $INSTANCE_SECURITY_GRP \
-                       --user-data file://$(pwd)/$INSTANCE_DATA_FILE --tag-specifications "[ { \"ResourceType\": \"instance\", \"Tags\": [ { \"Key\": \"Name\", \"Value\": \"${INSTANCE_NAME}\" } ] } ] " \
+new_instance_id=$($AWS --output json ec2 run-instances $INSTANCE_DRY_RUN --image-id  $INSTANCE_AMI_ID \
+                       --count $INSTANCE_COUNT --instance-type $INSTANCE_TYPE --key-name $INSTANCE_KEY_PAIR \
+                       --security-groups $INSTANCE_SECURITY_GRP --user-data file://$(pwd)/$INSTANCE_DATA_FILE \
+                       --tag-specifications "[ { \"ResourceType\": \"instance\", \"Tags\": [ { \"Key\": \"Name\", \"Value\": \"${INSTANCE_NAME}\" } ] } ] " \
                    | grep InstanceId  | tr -d ' ",' | awk -F: '{print $2}')
     
 echo "Instance created, summary:"
