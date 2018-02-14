8f0ff409291e5342009810648d38fd9517f10d62
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-14 12:59:49 -0200

    .

--- containers/rancher-server.run-aws-instance.sh
+++ containers/rancher-server.run-aws-instance.sh
@@ -130,6 +130,7 @@ done
 
 echo "Initializing instance..."
 new_instance_id=$($AWS --output json ec2 run-instances $INSTANCE_DRY_RUN --image-id  $INSTANCE_AMI_ID \
+                       --credit-specification CpuCredits=unlimited \
                        --count $INSTANCE_COUNT --instance-type $INSTANCE_TYPE --key-name $INSTANCE_KEY_PAIR \
                        --security-groups $INSTANCE_SECURITY_GRP --user-data file://$(pwd)/$INSTANCE_DATA_FILE \
                        --tag-specifications "[ { \"ResourceType\": \"instance\", \"Tags\": [ { \"Key\": \"Name\", \"Value\": \"${INSTANCE_NAME}\" } ] } ] " \
