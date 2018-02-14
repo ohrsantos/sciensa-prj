7952477bcb77596d3089c7d863e0d3144b6e7723
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-14 08:44:44 -0200

    .

--- containers/dev.sciensa-prj.run-aws-instance.sh
+++ containers/dev.sciensa-prj.run-aws-instance.sh
@@ -10,7 +10,7 @@ SCRIPT_NAME="dev.run-aws-instance"
 VERSION="0.19a"
 AUTHOR="Orlando Hehl Rebelo dos Santos"
 DATE_INI="14-01-2018"
-DATE_END="13-02-2018"
+DATE_END="14-02-2018"
 ################################################################################
 #Changes:
 #
@@ -47,7 +47,8 @@ SCIENSA_APP_CONTAINER="PUBLIC_DNS=NA APP_ENV=DEV docker run -d --rm -e APP_ENV -
 CPPCMS_CONTAINER="docker run -it --rm -u root -v /opt/cppcms  -p 3333:8080  --name dev-cppcms-docker ohrsan/cppcms:v1"
 
 TUTORIAL_APP_METEOR_CONTAINER_NAME=app-test3-meteor
-TUTORIAL_APP_METEOR_CONTAINER="docker run -d --rm  -p 3333:3000 -v ${TUTORIAL_APP_METEOR_CONTAINER_NAME}:/var/meteor --name ${TUTORIAL_APP_METEOR_CONTAINER_NAME} ohrsan/${TUTORIAL_APP_METEOR_CONTAINER_NAME}:1"
+TUTORIAL_APP_METEOR_CONTAINER="docker run -d --rm  -p 3333:3000 -v ${TUTORIAL_APP_METEOR_CONTAINER_NAME}:/var/meteor \
+                               --name ${TUTORIAL_APP_METEOR_CONTAINER_NAME} ohrsan/${TUTORIAL_APP_METEOR_CONTAINER_NAME}:1"
 
 ################################################################################
 # Macros:
@@ -96,10 +97,10 @@ user_data=(
 
 "docker login -u=ohrsan -p=bomdia01 >> /home/ec2-user/instance-creation.log 2>&1"
 "docker pull node:latest >> /home/ec2-user/instance-creation.log 2>&1"
-"${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
-#"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
-#"${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
-#"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+#"${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+"${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
 #"${TUTORIAL_APP_METEOR_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
 
 "chmod  g+rx /var/lib"
@@ -113,10 +114,10 @@ user_data=(
 "echo 'docker login -u=ohrsan -p=bomdia01 >> /home/ec2-user/instance-creation.log 2>&1' >> /etc/rc.d/rc.local"
 
 "echo 'docker pull node:latest >> /home/ec2-user/instance-creation.log 2>&1' >> /etc/rc.d/rc.local"
-"echo \"${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
-"echo \"#${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
-"echo \"#${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
-"echo \"#${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"#${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
 "echo \"#${TUTORIAL_APP_METEOR_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
 )
 
@@ -127,11 +128,14 @@ user_data=(
 > $INSTANCE_DATA_FILE
 for index in "${!user_data[@]}"; do 
     echo "${user_data[$index]}" >> $INSTANCE_DATA_FILE
-    echo "${user_data[$index]}" 
+    #echo "${user_data[$index]}" 
 done
 
 echo "Initializing instance..."
-new_instance_id=$($AWS --output json ec2 run-instances $INSTANCE_DRY_RUN --image-id  $INSTANCE_AMI_ID --count $INSTANCE_COUNT --instance-type $INSTANCE_TYPE --key-name $INSTANCE_KEY_PAIR --security-groups $INSTANCE_SECURITY_GRP --user-data file://$(pwd)/$INSTANCE_DATA_FILE --tag-specifications "[ { \"ResourceType\": \"instance\", \"Tags\": [ { \"Key\": \"Name\", \"Value\": \"${INSTANCE_NAME}\" } ] } ] " | grep InstanceId  | tr -d ' ",' | awk -F: '{print $2}')
+new_instance_id=$($AWS --output json ec2 run-instances $INSTANCE_DRY_RUN --image-id  $INSTANCE_AMI_ID --count $INSTANCE_COUNT \
+                       --instance-type $INSTANCE_TYPE --key-name $INSTANCE_KEY_PAIR --security-groups $INSTANCE_SECURITY_GRP \
+                       --user-data file://$(pwd)/$INSTANCE_DATA_FILE --tag-specifications "[ { \"ResourceType\": \"instance\", \"Tags\": [ { \"Key\": \"Name\", \"Value\": \"${INSTANCE_NAME}\" } ] } ] " \
+                   | grep InstanceId  | tr -d ' ",' | awk -F: '{print $2}')
     
 echo "Instance created, summary:"
 $AWS ec2 describe-instances --filters "Name=instance-id, Values=$new_instance_id"
@@ -140,7 +144,7 @@ echo "Waiting for the instance reach running state..."
 sleep 60
 
 echo "Attaching instances to their respective target groups..."
-$AWS  elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:us-east-1:606784160785:targetgroup/rancher-server/4c4da2ec95c5f5d7 --targets Id=$new_instance_id
+#$AWS  elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:us-east-1:606784160785:targetgroup/rancher-server/4c4da2ec95c5f5d7 --targets Id=$new_instance_id
 $AWS  elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:us-east-1:606784160785:targetgroup/jenkins-dev/49d5199a83cf3941 --targets Id=$new_instance_id
 $AWS  elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:us-east-1:606784160785:targetgroup/sciensa-dev/30895b398b8dd2bb --targets Id=$new_instance_id
 $AWS  elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:us-east-1:606784160785:targetgroup/cppcms-dev/4c9decd880e3678e  --targets Id=$new_instance_id
