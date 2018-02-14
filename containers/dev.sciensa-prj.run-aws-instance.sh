4188404db2d978d0042eb16a59b2df0e151ba52b
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-14 13:13:55 -0200

    .

--- containers/dev.sciensa-prj.run-aws-instance.sh
+++ containers/dev.sciensa-prj.run-aws-instance.sh
@@ -98,15 +98,15 @@ user_data=(
 "docker login -u=ohrsan -p=bomdia01 >> /home/ec2-user/instance-creation.log 2>&1"
 "docker pull node:latest >> /home/ec2-user/instance-creation.log 2>&1"
 #"${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
-"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+#"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
 "${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
-"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
+#"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
 #"${TUTORIAL_APP_METEOR_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1"
 
 "chmod  g+rx /var/lib"
 "chmod  g+rx /var/lib/docker"
 "chmod -R g+rx /var/lib/docker/volumes"
-"chown -R ec2-user:ec2-user /var/lib/docker/volumes/${TUTORIAL_APP_METEOR_CONTAINER_NAME}/_data"
+#"chown -R ec2-user:ec2-user /var/lib/docker/volumes/${TUTORIAL_APP_METEOR_CONTAINER_NAME}/_data"
 
 #Creating  /etc/rc.d/rc.local:
 "echo sleep 15 >> /etc/rc.d/rc.local"
@@ -115,9 +115,9 @@ user_data=(
 
 "echo 'docker pull node:latest >> /home/ec2-user/instance-creation.log 2>&1' >> /etc/rc.d/rc.local"
 "echo \"#${RANCHER_SERVER_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
-"echo \"${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"#${SCIENSA_APP_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
 "echo \"${JENKINS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
-"echo \"${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
+"echo \"#${CPPCMS_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
 "echo \"#${TUTORIAL_APP_METEOR_CONTAINER} >> /home/ec2-user/instance-creation.log 2>&1\" >> /etc/rc.d/rc.local"
 )
 
