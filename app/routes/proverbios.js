551fd354d296047d95f93c0569c28e382a6a0fdb
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-16 16:44:47 -0200

    Rancher plugin test

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.053a';
+    var APP_VERSION = '0.02.055a';
 
     function log_date_ip(req, path) {
             var ip;
