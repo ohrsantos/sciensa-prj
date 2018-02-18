79dc9563030c6a02bd16d009955c4e213e98635d
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-18 13:02:20 -0300

    Teste de webhook ... app ver 56

--- app/routes/proverbios.js
+++ app/routes/proverbios.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
     console.log('module.exports = function(app)');
 
-    var APP_VERSION = '0.02.055a';
+    var APP_VERSION = '0.02.056a';
 
     function log_date_ip(req, path) {
             var ip;
