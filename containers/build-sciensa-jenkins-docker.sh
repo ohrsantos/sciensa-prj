9e87097cf1742eabfeb353ffc34c73d27a4efec7
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-19 16:49:46 -0300

    Integrating CppCMS project and migrating to ohrs namespace

--- containers/build-sciensa-jenkins-docker.sh
+++ containers/build-sciensa-jenkins-docker.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 VERSION="latest"
-SCIENSA_JENKINS_HOME=/var/lib/docker/volumes/jenkins_docker_home/_data
+JENKINS_HOME=/var/lib/docker/volumes/jenkins_docker_home/_data
 WORK_DIR=$(pwd)
 
-cd $SCIENSA_JENKINS_HOME
+cd $JENKINS_HOME
 
 sudo rm -rf jobs/Node-App-DEV/nextBuildNumber jobs/Node-App-DEV/builds/*
 sudo rm -rf jobs/Node-App-PROD/nextBuildNumber jobs/Node-App-PROD/builds/*
 
-tar zcvf sciensa-jenkins.tar.gz *
+tar zcvf jenkins_home.tar.gz *
 
 #This version is for the case with need hidden files
-#tar zcvf sciensa-jenkins.tar.gz * .??*
+#tar zcvf jenkins_home.tar.gz * .??*
 
-mv sciensa-jenkins.tar.gz $WORK_DIR
+mv jenkins_home.tar.gz $WORK_DIR
 
 cd -
 
-docker build -f containers/Dockerfile.sciensa-jenkins-docker  -t ohrsan/sciensa-jenkins-docker:$VERSION .
+docker build -f containers/Dockerfile.ohrs-jenkins  -t ohrsan/ohrs-jenkins:$VERSION .
 
 docker login -u=ohrsan -p=bomdia01
 
-docker push ohrsan/sciensa-jenkins-docker:$VERSION
+docker push ohrsan/ohrs-jenkins:$VERSION
