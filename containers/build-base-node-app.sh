6946d13ba870738d4867c0b608bad927b058a29c
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-10-04 13:52:23 -0300

    .

--- containers/build-base-node-app.sh
+++ containers/build-base-node-app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+password=${1}
+
 docker build -f Dockerfile.base-node-app -t ohrsan/base-node-app .
 
-docker login -u=ohrsan -p=bomdia01
+docker login -u=ohrsan -p=$password
 
 docker push ohrsan/base-node-app
