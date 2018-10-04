78d9226a3f4b3ae3033b8c730e5828a4a939efda
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-10-04 13:50:52 -0300

    .

--- containers/build-jenkins-docker-v3.sh
+++ containers/build-jenkins-docker-v3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+password=${1}
+
 docker build -f Dockerfile.jenkins-docker-v3  -t ohrsan/jenkins-docker:v3 .
 
-docker login -u=ohrsan -p=bomdia01
+docker login -u=ohrsan -p=$password
 
 docker push ohrsan/jenkins-docker:v3
